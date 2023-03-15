b3ed453012c113394903c08a56d908646340f7d0
sett <raik.rohde@gmail.com>
2023-03-15 20:42:04 +0100

    feat: add cargo-quickinstall to rust.sh
    This change adds the cargo-quickinstall tool to rust.sh, allowing for simpler installation of Rust packages. It also updates the list of packages to install to include du-dust alongside existing packages sccache, coreutils, starship, exa, bat, ripgrep, tokei, miniserve, and fd-find.

--- rust.sh
+++ rust.sh
@@ -1,3 +1,4 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
-cargo install sccache coreutils starship exa bat ripgrep tokei miniserve fd-find turbocommit
+cargo install cargo-quickinstall
+cargo quickinstall sccache coreutils starship exa bat ripgrep tokei miniserve fd-find du-dust turbocommit
