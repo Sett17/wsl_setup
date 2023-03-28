f411bc858d17288cc056c80ea7681e2760407330
sett <raik.rohde@gmail.com>
2023-03-28 13:24:07 +0200

    feat: install wslu and add WIN_HOME variable
    This will install wslu and set the WIN_HOME environment variable to the Windows user profile directory.

--- bash_alias.sh
+++ bash_alias.sh
@@ -2,9 +2,6 @@ echo 'eval "$(~/.cargo/bin/starship init bash)"' > ~/.bash_aliases
 echo 'bind -f ~/.inputrc' >> ~/.bash_aliases
 echo " alias l='exa -l -a '
 alias lh='exa -l -s changed'
-alias goto='cd '
-alias root='/mnt/c/Users/sett'
-alias explorer='cmd.exe /c explorer .'
 alias lg='lazygit '
 alias v='vim '
 " >> ~/.bash_aliases
@@ -13,4 +10,5 @@ fastpush () { DATE=`date +"%D %T"`; git add . --all 2> /dev/null && git commit -
 ' >> ~/.bash_aliases
 echo "export EDITOR=vim" >> ~/.bash_aliases
 echo "export OPENAI_API_KEY=''" >> ~/.bash_aliases
+echo "export WIN_HOME=$(wslpath "$(wslvar USERPROFILE)")" >> ~/.bash_aliases
 source ~/.bashrc
--- basics.sh
+++ basics.sh
@@ -3,6 +3,6 @@ touch ~/.hushlogin
 sudo apt update
 sudo apt install python3-pip --assume-yes
 pip install --user git+https://github.com/cpbotha/xdg-open-wsl.git
-sudo apt install binwalk cmake pkg-config libssl-dev cmake zip --assume-yes
+sudo apt install binwalk cmake pkg-config libssl-dev cmake zip wslu --assume-yes
 
 curl -s "https://get.sdkman.io" | sudo bash
