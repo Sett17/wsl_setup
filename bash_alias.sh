1c27b976425d67ec04978eb0aacd10caf9882588
Sett17 <raik.rohde@gmail.com>
2023-06-21 02:37:45 +0200

    chore: update bash aliases
    Add new alias "wv" for opening files with wslview

--- bash_alias.sh
+++ bash_alias.sh
@@ -4,6 +4,7 @@ echo "alias l='exa -l'
 alias lh='exa -l -s changed'
 alias lg='lazygit '
 alias v='vim '
+alias wv='wslview '
 " >> ~/.bash_aliases
 echo 'mkcd () { mkdir -vp "$@" && cd "$@"; }
 fastpush () { DATE=`date +"%D %T"`; git add . --all 2> /dev/null && git commit --allow-empty -m "$DATE" && git push; }
