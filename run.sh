bd1aa11355f4729fef839734ad2cb55760ecae34
Sett17 <raik.rohde@gmail.com>
2023-06-21 02:37:31 +0200

    refactor: improve logging in run.sh
    Improve logging in the `execute_task` function in the `run.sh` file. Now, the task execution details are appended to the `tasks.log` file. Also, a timestamp is added at the start of the log.

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 execute_task() {
     task_name=$1
     if [ ! -f "did_${task_name}" ]; then
-        printf '\e[1;32mExecuting task %s\e[0m\n' "${task_name}"
-        ./${task_name}.sh
+        printf '\e[1;32mExecuting task %s\e[0m\n' "${task_name}" | tee -a tasks.log
+        ./${task_name}.sh >> tasks.log 2>&1
         touch "did_${task_name}"
     else
-        printf '\e[1;33mSkipping task %s\e[0m\n' "${task_name}"
+        printf '\e[1;33mSkipping task %s\e[0m\n' "${task_name}" | tee -a tasks.log
     fi
 }
 
+printf '\n\n\e[1;34mExecution started at %s\e[0m\n' "$(date)" | tee -a tasks.log
 execute_task "sudo"
 execute_task "basics"
 execute_task "rust"
