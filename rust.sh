557f8f5d1e0984b6c78d406fcc64671730f475f3
Raik Rohde <raik.rohde@gmail.com>
2023-08-28 18:08:20 +0200

    feat(rust.sh): add zoxide to the list of installed packages
    This commit adds the zoxide package to the list of packages installed using cargo binstall in the rust.sh script.

--- rust.sh
+++ rust.sh
@@ -2,4 +2,4 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-cargo binstall -y sccache coreutils exa bat starship ripgrep tokei miniserve fd-find turbocommit just
+cargo binstall -y sccache coreutils exa bat starship ripgrep tokei miniserve fd-find turbocommit just zoxide
