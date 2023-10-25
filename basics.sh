ec7a03b6509fb1e663c8326f53f0588868013580
Raik Rohde <raik.rohde@eviden.com>
2023-10-25 12:20:07 +0200

    feat: include system32 in PATH
    Add the system32 directory to the PATH environment variable in the ~/.bashrc file.

--- basics.sh
+++ basics.sh
@@ -6,3 +6,5 @@ pip install --user git+https://github.com/cpbotha/xdg-open-wsl.git
 sudo apt install binwalk cmake pkg-config libssl-dev cmake zip wslu --assume-yes
 
 curl -s "https://get.sdkman.io" | sudo bash
+
+echo "export PATH=$PATH:/mnt/c/Windows/System32" >> ~/.bashrc
