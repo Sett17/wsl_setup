917fd42b8be2d1de4996b1abe434b137d139e941
Raik Rohde <raik.rohde@eviden.com>
2023-11-06 11:25:02 +0100

    fix: fix quotes in tmux configuration file
    The quotes in the tmux configuration file were incorrectly using single quotes instead of double quotes. This commit fixes the quotes to ensure proper syntax.

--- tmux.sh
+++ tmux.sh
@@ -11,13 +11,13 @@ set-option status-style fg=brightblack
 set -g status-right-length 70
 set -g status-right "#(uptime -p | sed s/up// | sed s/,//g) #{=21:pane_title} %H:%M %d-%b-%y %A"
 
-set -g @plugin 'tmux-plugins/tpm'
-set -g @plugin 'tmux-plugins/tmux-resurrect'
-set -g @plugin 'tmux-plugins/tmux-continuum'
+set -g @plugin "tmux-plugins/tpm"
+set -g @plugin "tmux-plugins/tmux-resurrect"
+set -g @plugin "tmux-plugins/tmux-continuum"
 
-set -g @continuum-boot 'on'
-set -g @continuum-restore 'on'
-set -g @resurrect-capture-pane-contents 'on'
+set -g @continuum-boot "on"
+set -g @continuum-restore "on"
+set -g @resurrect-capture-pane-contents "on"
 
 unbind C-b
 set-option -g prefix C-Space
@@ -27,6 +27,7 @@ bind \\ split-window -h -c "#{pane_current_path}"
 bind - split-window -v -c "#{pane_current_path}"' > ~/.tmux.conf
 
 echo unbind \'\"\' >> ~/.tmux.conf
+
 echo 'unbind %
 
 bind r source-file ~/.tmux.conf
@@ -39,6 +40,6 @@ bind -n M-Down select-pane -D
 set -g mouse on
 set-environment -g PATH "/usr/local/bin:/bin:/usr/bin"
 
-set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
-set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
+set -as terminal-overrides ",*:Smulx=\E[4::%p1%dm"  # undercurl support
+set -as terminal-overrides ",*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m"  # underscore colours - needs tmux-3.0
 ' >> ~/.tmux.conf
